2d506dc8880336a44db60e582968bde50369ea75
Ninh Nguyen Duong <ndninh.040303@gmail.com>
2026-07-19 00:44:08 +0700

    feat: implement parseTimeString function

--- main.go
+++ main.go
@@ -2,7 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+func parseTimeString (tStr string) (time.Time, error) {
+	n := time.Now()
+	year := n.Year()
+	month := n.Month()
+	day := n.Day()
+	location := n.Location()
+
+	t, err := time.ParseInLocation("15:04", tStr, location)
+	if err != nil {
+		return time.Time{}, err
+	}
+	hour := t.Hour()
+	minute := t.Minute()
+	second := t.Second()
+	nanosecond := t.Nanosecond()
+
+	fullTime := time.Date(year, month, day, hour, minute, second, nanosecond, location)
+	
+	return fullTime, nil
+}
 
 func main() {
 	fmt.Println("Hello, World!")
